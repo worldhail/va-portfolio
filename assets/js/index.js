a11ff105d527d51c374cafdc89764b4b74d3e387
Jeric Chua <worldhail41@gmail.com>
2025-05-12 23:35:43 +0800

    Initial commits for va-portfolio

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+// MENU BUTTON EVENT LISTENER
+const menuBtn = document.querySelector('.menu-btn');
+const menuList = document.querySelector('.menu-list');
+menuBtn.addEventListener('click', () => {
+    menuList.classList.toggle('open-menu');
+    menuBtn.classList.toggle('slide-menu-btn');
+});
+
+// INITIAL BRIEF CONTEXT ON THE FRON PAGE
+const briefContext = document.querySelector('.brief-context');
+window.addEventListener('DOMContentLoaded', () => {
+    briefContext.classList.add('brief-context-scaleUp');
+});
