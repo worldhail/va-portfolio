4cbeff19e721ba7650b942e2f781e719ac735d4d
Jeric Chua <worldhail41@gmail.com>
2025-06-25 10:45:37 +0800

    Add animation when front page loads

--- docs/js/index.js
+++ docs/js/index.js
@@ -20,8 +20,12 @@ window.addEventListener('click', (e) => {
 
 // INITIAL BRIEF CONTEXT ON THE FRON PAGE
 const briefContext = document.querySelector('.brief-context');
+const frontPageImage = document.querySelector('.front-page-image');
+const briefContextContainer = document.querySelector('.brief-context-container');
 window.addEventListener('DOMContentLoaded', () => {
     briefContext.classList.add('brief-context-scaleUp');
+    frontPageImage.classList.add('front-page-image-slideLeft');
+    briefContextContainer.classList.add('brief-context-container-show');
 });
 
 // LOGO SLIDE UP
@@ -29,3 +33,19 @@ const logo = document.querySelector('.logo');
 window.addEventListener('scroll', () => {
     window.scrollY > 100 ? logo.classList.add('logo-slideUp') : logo.classList.remove('logo-slideUp');
 });
+
+// INTESECTION OBSERVER FOR EVERY SECTIONS
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            console.log('viewed')
+        }
+    })
+}, {
+    root: null,
+    // rootMargin: '0px',
+    threshold: 0.1
+});
+
+const target = document.querySelectorAll('section');
+target.forEach(section => observer.observe(section));
